c9fe11665bcab7483b74802e0190aa2d1f632834
Franck Dakia <dakiafranck@gmail.com>
2016-03-18 13:01:56 +0000

    Ajout d'autre entete-http

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -19,14 +19,20 @@ class Response
 	private static $header = [
 		200 => "OK",
 		201 => "Created",
+        204 => "No Content",
 		301 => "Moved Permanently",
 		302 => "Found",
 		304 => "Not Modified",
 		400 => "Bad Request",
 		401 => "Unauthorized",
-		404 => "Not Found",
 		403 => "Forbidden",
-		500 => "Internal Server Error"
+		404 => "Not Found",
+		405 => "Method Not Allowed",
+		408 => "Request Time Out",
+		500 => "Internal Server Error",
+		501	=> "Not Implemented",
+        503 => "Service Unavailable"
+
 	];
     
     /**
