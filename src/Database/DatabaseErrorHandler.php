4f92e2a23b05152c147a0b961bc99532d897357c
Franck Dakia <dakiafranck@gmail.com>
2016-03-30 12:44:47 +0000

    Ajout de finalisation du squette de DatabaseErrorHandler

---
+++ src/Database/DatabaseErrorHandler.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Bow\Database;
+
+use Bow\Exception\DatabaseException;
+
+class DatabaseErrorHandler
+{
+    /**
+     * @var array
+     */
+    private $pdoStatement;
+
+    /**
+     * @var string
+     */
+    private $driverCode = null;
+
+    /**
+     * @var string
+     */
+    private $driverMessage = null;
+
+    /**
+     * @var bool
+     */
+    private $error = false;
+
+    /**
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->pdoStatement = $config;
+        $this->formatError();
+    }
+
+    /**
+     * @return string
+     */
+    public function getSqlStateErrorCode()
+    {
+        return $this->pdoStatement[0];
+    }
+
+    /**
+     *
+     */
+    private function formatError()
+    {
+        if ($this->pdoStatement[1] !== null) {
+            $this->driverMessage = $this->pdoStatement[1];
+            $this->driverCode = $this->pdoStatement[0];
+            $this->error = true;
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function thereIsAnError()
+    {
+        return $this->error;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->driverMessage;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->pdoStatement;
+    }
+
+    /**
+     * @throws DatabaseException
+     */
+    public function throwError() {
+        throw new DatabaseException($this->getMessage(), E_USER_ERROR);
+    }
+}
