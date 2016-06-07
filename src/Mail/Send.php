e23d461594eae0668995415df3b8d4c044bf601c
Franck Dakia <dakiafranck@gmail.com>
2016-06-07 01:21:40 +0000

    Ajout de interface Send pour les classes Mail

---
+++ src/Mail/Send.php
@@ -0,0 +1,21 @@
+<?php
+namespace Bow\Mail;
+
+/**
+ * Interface Send
+ *
+ * @author Franck Dakia <dakiafranck@gmail.com>
+ * @package Bow\Mail
+ */
+interface Send
+{
+
+
+    /**
+     * send, envoie de mail.
+     *
+     * @param callable|null $cb
+     * @return mixed
+     */
+    public function send($cb = null);
+}
