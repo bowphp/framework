823c5a04d5cedb2fc76b093d7fd67e28f29ca036
Franck Dakia <dakiafranck@gmail.com>
2017-04-05 16:58:49 +0000

    Debut d'integration de Console

---
+++ src/Support/Console/Bow.php
@@ -0,0 +1,256 @@
+<?php
+namespace Bow\Support\Console;
+
+use Psy\Configuration;
+use Psy\Shell;
+
+class Bow
+{
+    /**
+     * @var array
+     */
+    private $serve_filename = './server.php';
+
+    /**
+     * @var array
+     */
+    private $bootstrap = ['public/index.php'];
+
+    /**
+     * @var string
+     */
+    private $dirname;
+
+    /**
+     * @var Command
+     */
+    private $_command;
+
+    /**
+     * Bow constructor.
+     * @param string $dirname
+     * @param Command $command
+     */
+    public function __construct($dirname, Command $command)
+    {
+        $this->dirname = $dirname;
+        $this->_command = $command;
+    }
+
+    /**
+     * Permet de lancer Bow task runner
+     */
+    public function run()
+    {
+        $this->call($this->_command->getParameter('command'));
+    }
+
+    /**
+     * Permet d'appeler un commande
+     *
+     * @param string $command
+     */
+    public function call($command)
+    {
+        if (! method_exists($this, $command)) {
+            $this->help();
+            exit(1);
+        }
+        call_user_func_array([$this, $command], []);
+    }
+
+    /**
+     * Permet de lancer un migration
+     */
+    public function migrate()
+    {
+        $action = $this->_command->getParameter('action');
+        $this->_command->$action();
+    }
+
+    /**
+     * Permet de créer des fichiers
+     */
+    public function create()
+    {
+        $action = $this->_command->getParameter('action');
+        if (! in_array($action, ['middleware', 'controller', 'model'])) {
+            $this->help('create');
+            exit(0);
+        }
+
+        $this->_command->$action($this->_command->getParameter('target'));
+    }
+
+    /**
+     * Permet de lancer le serveur local
+     */
+    public function serve()
+    {
+        $port = (int) $this->_command->options('--port', 5000);
+        $hostname = $this->_command->options('--host', 'localhost');
+        $settings = $this->_command->getParameter('--php-settings', '');
+
+        // resource.
+        $r = fopen("php://stdout", "w");
+        if ($r) {
+            fwrite($r, sprintf("[%s] web server start at http://localhost:%s \033[0;31;7mctrl-c for shutdown it\033[00m\n", date('F d Y H:i:s a'), $port));
+        }
+
+        fclose($r);
+        // lancement du serveur.
+        return shell_exec("php -S $hostname:$port server.php $settings");
+    }
+
+    /**
+     * Permet de lancer le repl
+     */
+    public function console()
+    {
+        if (isset($argv[1]) && $argv[1] == 'help') {
+            $this->help('console');
+            return;
+        }
+
+        if (! class_exists('\Psy\Shell')) {
+            echo 'SVP installez psy/psysh:@stable avec la commande "composer require --dev psy/psysh @stable"';
+            return;
+        }
+
+        $shell = new Shell(new Configuration());
+        $shell->setIncludes($this->bootstrap);
+        $shell->run();
+        return;
+    }
+
+    /**
+     * Permet de generate un resource sur un controller
+     */
+    public function generate()
+    {
+        $this->_command->getParameter('action');
+    }
+
+    /**
+     * Permet de changer les fichiers de demarage
+     *
+     * @param array $bootstrap
+     */
+    public function setBootstrap(array $bootstrap)
+    {
+        $this->bootstrap = $bootstrap;
+    }
+
+    /**
+     * Permet de changer les fichiers de demarage
+     *
+     * @param string $serve_filename
+     */
+    public function setServerFilename($serve_filename)
+    {
+        $this->serve_filename = $serve_filename;
+    }
+
+    /**
+     * Display global help or helper command.
+     *
+     * @param string|null $command
+     * @return int
+     */
+    private function help($command = null)
+    {
+        if ($command === null) {
+            $usage = <<<USAGE
+
+Bow usage: php bow command:action [name] [help|--with-model|--no-plain|--create|--table|--seed]
+
+\033[0;31mcommand\033[00m:
+
+ \033[0;33mhelp\033[00m display command helper
+
+ \033[0;32mgenerate\033[00m create a new app key and resources
+  option:
+   \033[0;33mgenerate:resource\033[00m  Create new REST assicate at a controller
+   \033[0;33mgenerate:key\033[00m       Create new app key
+
+ \033[0;32mmake\033[00m                 Create a user class
+  option:
+   \033[0;33mmake:middleware\033[00m    Create new middleware
+   \033[0;33mmake:controller\033[00m    Create new controller
+   \033[0;33mmake:model\033[00m         Create new model
+
+ \033[0;32mmigrate\033[00m apply a migration in user model
+  option:
+   \033[0;33mmigrate:make\033[00m       Create a new migration
+   \033[0;33mmigrate:down\033[00m       Drop migration
+   \033[0;33mmigrate:up\033[00m         Update or create table of the migration
+
+ \033[0;32mconsole\033[00m show psysh php REPL for debug you code.
+ \033[0;32mserver\033[00m run a local web server.
+
+USAGE;
+            echo $usage;
+            return 0;
+        }
+
+        switch($command) {
+            case 'help':
+                echo "\033[0;33mhelp\033[00m display command helper\n";
+                break;
+            case 'create':
+                echo <<<U
+\n\033[0;32mcreate\033[00m create a user class\n
+    [option]
+    --with-model[=name]  Create a model associte at controller
+    --no-plain              Create a plain controller
+
+    * you can use --no-plain --with-model
+
+    \033[0;33m$\033[00m php \033[0;34mbow\033[00m create:controller name [option]  For create a new controlleur
+    \033[0;33m$\033[00m php \033[0;34mbow\033[00m create:middleware name           For create a new middleware
+    \033[0;33m$\033[00m php \033[0;34mbow\033[00m create:model name                For create a new model
+    \033[0;33m$\033[00m php \033[0;34mbow\033[00m create help                      For display this
+
+U;
+
+                break;
+            case 'generate':
+
+                echo <<<U
+    \n\033[0;32mgenerate\033[00m create a resource and app keyn
+    \033[0;33m$\033[00m php \033[0;34mbow\033[00m generate:resource name             For create a new REST controller
+    \033[0;33m$\033[00m php \033[0;34mbow\033[00m generate:key                       For generate a new APP KEY
+    \033[0;33m$\033[00m php \033[0;34mbow\033[00m generate help                      For display this
+
+U;
+                break;
+            case 'migrate':
+                echo <<<U
+\n\033[0;32mmigrate\033[00m apply a migration in user model\n
+    [option]
+    --seed[--seed=n]      Fill table for n value
+    --create=table_name   Change name of table
+    --table=table_name    Alter migration table
+
+    \033[0;33m$\033[00m php \033[0;34mbow\033[00m migrate:make name [option]     Create a new migration
+    \033[0;33m$\033[00m php \033[0;34mbow\033[00m migrate:up name [option]       Up the specify migration
+    \033[0;33m$\033[00m php \033[0;34mbow\033[00m migrate:down name              Down migration
+    \033[0;33m$\033[00m php \033[0;34mbow\033[00m migrate [option]               Up all defined migration
+    \033[0;33m$\033[00m php \033[0;34mbow\033[00m migrate help                   For display this
+
+U;
+
+                break;
+
+            case 'console':
+                echo <<<U
+\n\033[0;32mconsole\033[00m show psysh php REPL\n
+    php bow console
+    >>> //test you code here.
+U;
+                break;
+        }
+
+        return 0;
+    }
+}
---
+++ src/Support/Console/Command.php
@@ -0,0 +1,635 @@
+<?php
+namespace Bow\Support\Console;
+
+
+use Bow\Resource\Storage;
+use Bow\Support\Collection;
+
+class Command
+{
+    /**
+     * @var string
+     */
+    const BAD_COMMAND = "Bad command.%sPlease type \033[0;32;7m`php bow help or php bow command help` for more information.";
+
+    /**
+     * @var string
+     */
+    private $dirname;
+
+    /**
+     * @var array
+     */
+    private $options = [];
+
+    /**
+     * Command constructor.
+     */
+    public function __construct($dirname)
+    {
+        $this->dirname = $dirname;
+        $this->formatParameters();
+    }
+
+    /**
+     * Permet de formater les options
+     */
+    public function formatParameters()
+    {
+        foreach ($GLOBALS['argv'] as $key => $param) {
+            if ($key == 0) {
+                continue;
+            }
+            if ($key == 1) {
+                if (preg_match('/^[a-z]+:[a-z]+$/', $param)) {
+                    $part = explode(':', $param);
+                    $this->options['command'] = $part[0];
+                    $this->options['action'] = $part[1];
+                    continue;
+                }
+                $this->options['command'] = $param;
+                continue;
+            }
+            if ($key == 2) {
+                if (preg_match('/^[a-z_-]+$/', $param)) {
+                    $this->options['target'] = $param;
+                    continue;
+                }
+            }
+            if (preg_match('/^--[a-z-]+$/', $param)) {
+                $this->options['options'][$param] = '';
+                continue;
+            }
+            if (count($part = explode('=', $param)) == 2) {
+                $this->options['options'][$part[0]] = $part[1];
+                continue;
+            }
+            $this->options['trush'][] = $param;
+        }
+
+        if (isset($this->options['options'])) {
+            $this->options['options'] = new Collection($this->options['options']);
+        }
+    }
+
+
+    /**
+     * Permet de récupérer un parametre
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return mixed|Collection|null
+     */
+    public function getParameter($key, $default = null)
+    {
+        return isset($this->options[$key]) ? $this->options[$key] : $default;
+    }
+
+    /**
+     * Permet de récupérer les options de la commande
+     *
+     * @param string $key
+     * @param string $default
+     * @return Collection|mixed|null
+     */
+    public function options($key = null, $default = null)
+    {
+        $option = $this->getParameter('options', new Collection());
+
+        if ($key == null) {
+            return $option;
+        }
+
+        return $option->get($key, $default);
+    }
+
+
+    /**
+     * Permet de monter un migration
+     *
+     * @param string $model
+     */
+    public function up($model)
+    {
+        $this->makeMigration($model, 'up');
+    }
+
+    /**
+     * Permet supprimer une migration dans la base de donnée
+     *
+     * @param string $model
+     */
+    public function down($model)
+    {
+        $this->makeMigration($model, 'down');
+    }
+
+    /**
+     * @param $model
+     * @param $type
+     * @throws \ErrorException
+     */
+    private function makeMigration($model, $type)
+    {
+        if ($model) {
+            $fileParten = $this->dirname.strtolower("/migration/*{$model}*.php");
+        } else {
+            $fileParten = $this->dirname.strtolower("/migration/*.php");
+        }
+
+        if ($model == null) {
+            $type = "up";
+        }
+
+        $register = ["file" => [], "tables" => []];
+
+        foreach(file($this->dirname."/migration/.registers") as $r) {
+            $tmp = explode("|", $r);
+            $register["file"][] = $tmp[0];
+            $register["tables"][] = $tmp[1];
+        }
+
+        foreach(glob($fileParten) as $file) {
+            if (! file_exists($file)) {
+                throw new \ErrorException("$file n'existe pas.", E_USER_ERROR);
+            }
+
+            // Collection des fichiers de migration.
+            $filename = preg_replace("@^(" . $this->dirname."/migration/)|(\.php)$@", "", $file);
+
+            if (in_array($filename, $register["file"])) {
+                $num = array_flip($register["file"])[$filename];
+                $model = rtrim($register["tables"][$num]);
+            }
+
+            require $file;
+
+            // Formatage de la classe et Execution de la methode up ou down
+            $class = "Create".ucfirst($model)."Table";
+            $instance = new $class;
+            call_user_func([$instance, strtolower($type)]);
+            $options = $this->getParameter('options');
+
+            if ($options == null) {
+                return;
+            }
+
+            if ($options->has('--seed')) {
+                $n = (int) $options->get('--seed', 1);
+                $r = call_user_func_array([$instance, 'fill'], [$n]);
+                $s = $r > 1 ? 's' : '';
+                echo "\033[0;33m$r\033[00m \033[0;32mseed$s in \033[00m[$model] \033[0;32mmigration\033[00m\n";
+            }
+        }
+    }
+
+    /**
+     * Permet de create une migration
+     *
+     * @param $model
+     * @throws \ErrorException
+     */
+    public function make($model)
+    {
+        $mapMethod = ["create", "drop"];
+        $table = $model;
+
+        if ($this->getParameter('options') == null) {
+            throw new \ErrorException(sprintf(self::BAD_COMMAND, ''));
+        }
+
+        $options = $this->getParameter('options');
+
+        if ($options->has('--table')) {
+            if ($options->get('--table') == true) {
+                throw new \ErrorException(sprintf(self::BAD_COMMAND, ' [--table] '));
+            }
+            $table = $options->get('--table');
+            $mapMethod = ["table", "drop"];
+        }
+        if ($options->has('--create')) {
+            if ($options->get('--create') == true) {
+                throw new \ErrorException(sprintf(self::BAD_COMMAND, ' [--create] '));
+            }
+            $table = $options->get('--create');
+            $mapMethod = ["create", "drop"];
+        }
+
+
+        $class_name = ucfirst($table);
+        $migrate = <<<doc
+<?php
+use \Bow\Database\Migration\Fields;
+use \Bow\Database\Migration\Schema;
+use \Bow\Database\Migration\Migration;
+
+class Create{$class_name}Table extends Migration
+{
+    /**
+     * create Table
+     *
+     * @param bool \$display
+     */
+    public function up()
+    {
+        Schema::{$mapMethod[0]}("$table", function(Fields \$table) {
+            \$table->increment('id');
+            \$table->timestamps();
+        });
+    }
+
+    /**
+     * Drop Table
+     */
+    public function down()
+    {
+        Schema::{$mapMethod[1]}("$table");
+    }
+}
+doc;
+        if (preg_match("/^[a-z]+$/", $model)) {
+            $model = "create_{$model}_table";
+        }
+
+        $createAt = date("Y_m_d") . "_" . date("His");
+        $file = $this->dirname."/migration/${createAt}_${model}.php";
+        file_put_contents($file, $migrate);
+        Storage::append($this->dirname."/migration/.registers", "{$createAt}_{$model}|$table\n");
+
+        echo "\033[0;32mmigration file \033[00m[$model]\033[0;32m created.\033[00m\n";
+        return;
+    }
+
+    /**
+     * Permet de mettre en place le systeme de resource.
+     * @param string $controller_name
+     */
+    public function resource($controller_name)
+    {
+        $path = preg_replace("/controller/", "", strtolower($controller_name));
+        $model = ucfirst($path);
+        $modelNamespace = '';
+
+        if (static::readline("Voulez vous que je crée les vues associées?")) {
+            $model = strtolower($model);
+            @mkdir($this->dirname."/app/views/".$model, 0766);
+
+            echo "\033[0;33;7m";
+            foreach(["create", "edit", "show", "index", "update", "delete"] as $value) {
+                $file = $this->dirname."/app/views/$model/$value.twig";
+                file_put_contents($file, "{# Vue '$value' du model '$model' #}");
+                echo "$file\n";
+            }
+            echo "\033[00m";
+        }
+
+        if (static::readline("Voulez vous que je crée un model?")) {
+            if (isset($option[0]) && $option[0] == '--model') {
+                if (isset($option[1])) {
+                    $model = $option[1];
+                } else {
+                    echo "\033[0;32;7mLe nom du model non spécifié --model=model_name.\033[00m\n";
+                }
+            }
+
+            $this->model($model);
+            $modelNamespace = "\nuse App\\" . ucfirst($model);
+
+            if ($this->readline('Voulez vous que je crée une migration pour ce model? ')) {
+                $this->make($model);
+            }
+        }
+
+        $controllerRestTemplate =<<<CC
+<?php
+namespace App\Controllers;
+$modelNamespace
+use Bow\Database\Database;
+
+class {$controller_name} extends Controller
+{
+    /**
+     * Créer une nouvelle instance du controller
+     */
+    public function __construct()
+    {
+        \$this->middleware("crsf");
+    }
+
+    /**
+     * Point d'entré
+     * GET /$path
+     *
+     * @param mixed \$id [optional] L'identifiant de l'element à récupérer
+     * @return mixed
+     */
+    public function index(\$id = null)
+    {
+        // code ici
+    }
+
+    /**
+     * Permet d'afficher la vue permettant de créer une résource.
+     *
+     * GET /$path/create
+     */
+    public function create()
+    {
+        // code ici
+    }
+
+    /**
+     * Permet d'ajouter une nouvelle résource dans la base d'information
+     *
+     * POST /$path
+     */
+    public function store()
+    {
+        // code ici
+    }
+
+    /**
+     * Permet de récupérer un information précise avec un identifiant.
+     *
+     * GET /$path/:id
+     *
+     * @param mixed \$id L'identifiant de l'élément à récupérer
+     * @return mixed
+     */
+    public function show(\$id)
+    {
+        // code ici
+    }
+
+    /**
+     * Mise à jour d'un résource en utilisant paramètre du GET
+     *
+     * GET /$path/:id/edit
+     *
+     * @param mixed \$id L'identifiant de l'élément à mettre à jour
+     * @return mixed
+     */
+    public function edit(\$id)
+    {
+        // code ici
+    }
+
+    /**
+     * Mise à jour d'une résource
+     *
+     * PUT /$path/:id
+     *
+     * @param mixed \$id L'identifiant de l'élément à mettre à jour
+     * @return mixed
+     */
+    public function update(\$id)
+    {
+        // code ici
+    }
+
+    /**
+     * Permet de supprimer une resource
+     *
+     * DELETE /$path/:id
+     *
+     * @param mixed \$id L'identifiant de l'élément à supprimer
+     * @return mixed
+     */
+    public function destroy(\$id)
+    {
+        // code ici
+    }
+}
+CC;
+        file_put_contents($this->dirname."/app/Controllers/${controller_name}.php", $controllerRestTemplate);
+        echo "\033[0;32mcontroller created \033[00m[{$controller_name}]\033[0;32m\033[00m\n";
+        return 0;
+    }
+
+    /**
+     * Create new controller file
+     *
+     * @param string $controller_name
+     */
+    public function controller($controller_name)
+    {
+        if (! preg_match("/controller$/i", $controller_name)) {
+            $controller_name = ucfirst($controller_name) . "Controller";
+        } else {
+            if (preg_match("/^(.+)(controller)$/", $controller_name, $match)) {
+                array_shift($match);
+                $controller_name = ucfirst($match[0]) . ucfirst($match[1]);
+            } else {
+                $controller_name = ucfirst($controller_name);
+            }
+        }
+
+        if (file_exists($this->dirname."/app/Controllers/$controller_name.php")) {
+            echo "\033[0;31mcontroller \033[0;33m\033[0;31m[$controller_name]\033[00m\033[0;31m already exist.\033[00m\n";
+            return;
+        }
+
+        if ($this->options('--no-plain')) {
+
+            $content = <<<CONTENT
+    /**
+     * Point d'entré de l'application
+     *
+     * @param mixed \$id [optional] L'identifiant de l'élément à récupérer
+     * @return mixed
+     */
+    public function index(\$id = null)
+    {
+        // code ici
+    }
+
+    /**
+     * Permet d'afficher la vue permettant de créer une résource.
+     */
+    public function create()
+    {
+        // code ici
+    }
+
+    /**
+     * Permet d 'ajouter une nouvelle résource dans la base d'information
+     */
+    public function store()
+    {
+        // code ici
+    }
+
+    /**
+     * Permet de récupérer un information précise avec un identifiant.
+     *
+     * @param mixed \$id L'identifiant de l'élément à récupérer
+     * @return mixed
+     */
+    public function show(\$id)
+    {
+        // code ici
+    }
+
+    /**
+     * Mise à jour d'un résource en utilisant paramètre du GET
+     *
+     * @param mixed \$id L'identifiant de l'élément à mettre à jour
+     * @return mixed
+     */
+    public function edit(\$id)
+    {
+        // code ici
+    }
+
+    /**
+     * Mise à jour d'une résource
+     *
+     * @param mixed \$id L'identifiant de l'élément à mettre à jour
+     * @return mixed
+     */
+    public function update(\$id)
+    {
+        // code ici
+    }
+
+    /**
+     * Permet de supprimer une resource
+     *
+     * @param mixed \$id L'identifiant de l'élément à supprimer
+     * @return mixed
+     */
+    public function destroy(\$id)
+    {
+        // code ici
+    }
+CONTENT;
+        } else {
+            $content = <<<CONTENT
+    '// Écrivez votre code ici.';
+CONTENT;
+        }
+
+        $controller_template =<<<CC
+<?php
+namespace App\Controllers;
+
+class {$controller_name} extends Controller
+{
+    $content
+}
+CC;
+        file_put_contents($this->dirname."/app/Controllers/${controller_name}.php", $controller_template);
+    }
+
+    /**
+     * @param $middleware_name
+     * @return int
+     */
+    public function middleware($middleware_name)
+    {
+        $middleware_name = ucfirst($middleware_name);
+
+        if (file_exists($this->dirname."/app/Middleware/$middleware_name.php")) {
+            echo "\033[0;31mmiddleware \033[0;33m\033[0;31m[$middleware_name]\033[00m\033[0;31m already exist.\033[00m\n";
+            return 0;
+        }
+
+        $middleware_template = <<<CM
+<?php
+namespace App\Middleware;
+
+use \Bow\Http\Request;
+
+class {$middleware_name}
+{
+    /**
+     * Fonction de lancement du middleware.
+     * 
+     * @param \\Bow\\Http\\Request \$request
+     * @param \\Closure \$next
+     * @return boolean
+     */
+    public function handle(Request \$request, \\Closure \$next)
+    {
+        // code ici
+        return \$next();
+    }
+}
+CM;
+        file_put_contents($this->dirname."/app/Middleware/$middleware_name.php", $middleware_template);
+        echo "\033[0;32mmiddleware \033[00m[{$middleware_name}]\033[0;32m created.\033[00m\n";
+
+        return 0;
+    }
+
+    /**
+     * Create new model file
+     *
+     * @param string $model_name
+     * @param string|null $table_name
+     * @return int
+     */
+    public function model($model_name, $table_name = null)
+    {
+        $model_name = ucfirst($model_name);
+        if (is_string($table_name)) {
+            $table_name = strtolower($table_name);
+        } else {
+            $table_name = strtolower($model_name);
+        }
+
+        $model = <<<MODEL
+<?php
+namespace App;
+
+use Bow\Database\Model;
+
+class ${model_name} extends Model
+{
+    /**
+     * Le nom de la table.
+     *
+     * @var string
+     */
+    protected static \$table = "$table_name";
+}
+
+MODEL;
+        if (file_exists($this->dirname."/app/${model_name}.php")) {
+            echo "\033[0;33mmodel \033[0;33m\033[0;31m[${model_name}]\033[00m\033[0;31m already exist.\033[00m\n";
+            return 0;
+        }
+
+        file_put_contents($this->dirname."/app/${model_name}.php", $model);
+
+        echo "\033[0;32mmodel created \033[00m[${model_name}]\033[0;32m\033[00m\n";
+        return 0;
+    }
+
+    public function key()
+    {
+
+    }
+
+    /**
+     * Read ligne
+     *
+     * @param string $message
+     * @return bool
+     */
+    private function readline($message)
+    {
+        echo "\033[0;32m$message y/N\033[00m >>> ";
+        $input = readline();
+
+        if (strtolower($input) == "y") {
+            return true;
+        }
+
+        if (strtolower($input) == 'n' || $input == '') {
+            return false;
+        }
+
+        return false;
+    }
+}
