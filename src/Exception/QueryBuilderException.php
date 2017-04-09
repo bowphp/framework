7e0aa35598cebc6cc4362a93968015118629c718
Franck Dakia <dakiafranck@gmail.com>
2017-04-09 14:21:50 +0000

    Ajout exception pour QueryBuilder

---
+++ src/Exception/QueryBuilderException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: papac
+ * Date: 4/8/17
+ * Time: 7:21 PM
+ */
+
+namespace Bow\Exception;
+
+
+class QueryBuilderException extends \ErrorException {}
