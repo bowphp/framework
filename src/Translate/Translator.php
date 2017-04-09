cc84d738103753af4097a7ebd117f5ed7a2ba360
Franck Dakia <dakiafranck@gmail.com>
2017-04-09 14:22:15 +0000

    Initialisation de la classe de translate

---
+++ src/Translate/Translator.php
@@ -0,0 +1,30 @@
+<?php
+namespace Bow\Translate;
+
+class Translator
+{
+    /**
+     * Permet de faire la tranduction
+     *
+     * @param string $translation
+     * @param array $data
+     * @param string $local
+     * @return string
+     */
+    public static function make($translation, array $data = [], $local = null)
+    {
+        return '';
+    }
+
+    /**
+     * Permet de charge
+     *
+     * @param string $source
+     */
+    public function loadTranslateSource($source)
+    {
+        if (true) {
+
+        }
+    }
+}
