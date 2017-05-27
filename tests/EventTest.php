a42a755d6de45c1d25f03bc1a5af22fa5d7f4ca7
Franck Dakia <dakiafranck@gmail.com>
2017-05-27 14:28:11 +0000

    Ajout de test unitaire

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -2,8 +2,33 @@
 
 use Bow\Event\Event;
 
+class EventTable extends \Bow\Database\Model
+{
+    protected $table = 'pets';
+
+    public function __construct(array $data = [])
+    {
+        parent::__construct($data);
+        EventTable::created(function() {
+            echo 'Created';
+        });
+        EventTable::deleted(function() {
+            echo 'Deleted';
+        });
+        EventTable::updated(function() {
+            echo 'Updated';
+        });
+    }
+}
+
 class EventTest extends \PHPUnit\Framework\TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+        statement('create table if not exists pets (id int, name varchar(255));');
+    }
+
     public function testAddEvent()
     {
         Event::on('user.destroy', function($name) {
@@ -23,5 +48,31 @@ class EventTest extends \PHPUnit\Framework\TestCase
     public function testEventEmit2()
     {
         Event::emit('user.destroy', 'destroy');
+    }
+
+    public function testModelCreated()
+    {
+        $pets = new EventTable();
+        $this->assertInstanceOf(EventTable::class, $pets);
+        $pets->setAttributes([
+            'id' => 1,
+            'name' => 'Filou'
+        ]);
+        $this->assertEquals($pets->save(), 1);
+    }
+
+    public function testModelUpdated()
+    {
+        $pets = EventTable::find(1);
+        $this->assertInstanceOf(EventTable::class, $pets);
+        $pets->name = 'Loulou';
+        $this->assertEquals($pets->save(), 1);
+    }
+
+    public function testModelDeleted()
+    {
+        $pets = EventTable::find(1);
+        $this->assertInstanceOf(EventTable::class, $pets);
+        $this->assertEquals($pets->delete(), 1);
     }
 }
