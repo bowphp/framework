a542525a1c80c2e052304571ccb6fe2b08a5eebb
Franck DAKIA <dakiafranck@gmail.com>
2018-05-11 15:20:14 +0000

    fix phpunit test autoloading

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__."/../../vendor/autoload.php";
+require __DIR__."/../vendor/autoload.php";
 
 Bow\Config\Config::configure(__DIR__.'/config');
 Bow\Application\Actionner::configure([], []);
