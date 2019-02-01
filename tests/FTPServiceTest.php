8dfbb6570836e297996e5b4bbdfcd830767ad108
Salomon Dion <dev.mrdion@gmail.com>
2019-02-01 14:36:26 +0000

    [storage] update tests

--- tests/FTPServiceTest.php
+++ tests/FTPServiceTest.php
@@ -1,19 +1,28 @@
 <?php
 
 
+use Bow\Storage\Storage;
+
 class FTPServiceTest extends \PHPUnit\Framework\TestCase
 {
     protected function setUp()
     {
-        \Bow\Storage\Storage::configure(require 'config/resource.php');
+        Storage::configure(require 'config/resource.php');
+        Storage::pushService(['ftp' => \Bow\Storage\Service\FTPService::class]);
     }
 
     public function testUnsecureConnection()
     {
-        Bow\Storage\Storage::pushService(['ftp' => \Bow\Storage\Service\FTPService::class]);
         /** @var \Bow\Storage\Service\FTPService $ftp_service_instance */
-        $ftp_service_instance = \Bow\Storage\Storage::service('ftp');
+        $ftp_service_instance = Storage::service('ftp');
         $this->assertInstanceOf(\Bow\Storage\Service\FTPService::class, $ftp_service_instance);
         $this->assertNotFalse($ftp_service_instance::getConnection());
+    }
+
+    public function testHasCorrectRootFolder()
+    {
+        $config = require 'config/resource.php';
+        $ftp_service_instance = Storage::service('ftp');
+        $this->assertEquals($ftp_service_instance->getCurrentDir(), $config['ftp']['root']);
     }
 }
