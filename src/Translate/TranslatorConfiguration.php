61597fccd89720ddcbaeb6204c8317f63a60a12b
Franck DAKIA <dakiafranck@gmail.com>
2019-03-30 17:49:01 +0000

    [translate] change the config key

--- src/Translate/TranslatorConfiguration.php
+++ src/Translate/TranslatorConfiguration.php
@@ -12,14 +12,14 @@ class TranslatorConfiguration extends Configuration
      */
     public function create(Loader $config)
     {
-        $this->container->bind('trans', function () use ($config) {
-            $auto_detected = is_null($config['trans.auto_detected'])
+        $this->container->bind('translate', function () use ($config) {
+            $auto_detected = is_null($config['translate.auto_detected'])
                 ? false
-                : $config['trans.auto_detected'];
+                : $config['translate.auto_detected'];
 
             return Translator::configure(
-                $config['trans.lang'],
-                $config['trans.dictionary'],
+                $config['translate.lang'],
+                $config['translate.dictionary'],
                 $auto_detected
             );
         });
@@ -30,6 +30,6 @@ class TranslatorConfiguration extends Configuration
      */
     public function run()
     {
-        $this->container->make('trans');
+        $this->container->make('translate');
     }
 }
