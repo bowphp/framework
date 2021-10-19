883ddff14aeaee60c194a9ee628edab3218a844d
Franck DAKIA <dakiafranck@gmail.com>
2021-10-19 13:06:27 +0000

    Remnane validation command

--- src/Console/Command/ValidationCommand.php
+++ src/Console/Command/ValidationCommand.php
@@ -5,33 +5,33 @@ namespace Bow\Console\Command;
 use Bow\Console\Color;
 use Bow\Console\Generator;
 
-class ValidatorCommand extends AbstractCommand
+class ValidationCommand extends AbstractCommand
 {
     /**
-     * Add validator
+     * Add validation
      *
-     * @param string $validator
+     * @param string $validation
      *
      * @return int
      */
-    public function generate($validator)
+    public function generate(string $validation)
     {
         $generator = new Generator(
             $this->setting->getValidationDirectory(),
-            $validator
+            $validation
         );
 
         if ($generator->fileExists()) {
-            echo Color::red('The validator already exists.');
+            echo Color::red('The validation already exists.');
 
             exit(0);
         }
 
-        $generator->write('validator', [
+        $generator->write('validation', [
             'baseNamespace' => $this->namespaces['validation']
         ]);
 
-        echo Color::green('The validator was created well.');
+        echo Color::green('The validation was created well.');
 
         exit(0);
     }
