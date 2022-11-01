c145417804f6a9bf395ee8431a468c6ff0749e20
Franck DAKIA <dakiafranck@gmail.com>
2022-11-01 09:06:02 +0000

    Fixes the cache unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 
-use Bow\Tests\Config\TestingConfiguration;
-
-define('TESTING_RESOURCE_BASE_DIRECTORY', sprintf('%s', sys_get_temp_dir()));
+define('TESTING_RESOURCE_BASE_DIRECTORY', sprintf('%s/bowphp_testing', sys_get_temp_dir()));
 
 require __DIR__."/../vendor/autoload.php";
-
-Bow\Container\Action::configure([], []);
