aa6f8711d4b7ae69a33038d19fff81872acd8b70
Franck DAKIA <dakiafranck@gmail.com>
2022-11-01 15:36:41 +0000

    Add unity tests for generate by writting file

--- tests/Console/GeneratorTest.php
+++ tests/Console/GeneratorTest.php
@@ -8,7 +8,6 @@ class GeneratorTest extends \PHPUnit\Framework\TestCase
     public function test_generate_stubs()
     {
         $generator = new Generator(TESTING_RESOURCE_BASE_DIRECTORY, 'CreateUserCommand');
-
         $content = $generator->makeStubContent('command', [
             "namespace" => "",
             "className" => "CreateUserCommand",
@@ -23,11 +22,21 @@ class GeneratorTest extends \PHPUnit\Framework\TestCase
     public function test_generate_stub_without_data()
     {
         $generator = new Generator(TESTING_RESOURCE_BASE_DIRECTORY, 'CreateUserCommand');
-
         $content = $generator->makeStubContent('command', []);
 
         $this->assertNotNull($content);
         $this->assertRegExp("@\nnamespace\s\{baseNamespace\}\{namespace\};\n@", $content);
         $this->assertRegExp("@\nclass\s\{className\}\sextends\sConsoleCommand\n@", $content);
+    }
+
+    public function test_generate_by_writing_file()
+    {
+        $generator = new Generator(TESTING_RESOURCE_BASE_DIRECTORY, 'CreateUserCommand');
+        $result = $generator->write('command', [
+            "baseNamespace" => "Generator\Testing",
+        ]);
+
+        $this->assertTrue($result);
+        $this->assertTrue(file_exists($generator->getPath()));
     }
 }
