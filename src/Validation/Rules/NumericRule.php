b005137307ac43eec02e0ac9dcae7c92795ebc0f
Franck DAKIA <dakiafranck@gmail.com>
2022-11-01 08:58:17 +0000

    Refactoring data validation package

---
+++ src/Validation/Rules/NumericRule.php
@@ -0,0 +1,93 @@
+<?php declare(strict_types=1);
+
+namespace Bow\Validation\Rules;
+
+trait NumericRule
+{
+    /**
+     * Compile Number Rule
+     *
+     * [number] Check that the contents of the field is a number
+     *
+     * @param string $key
+     * @param string $masque
+     * @return void
+     */
+    protected function compileNumber($key, $masque)
+    {
+        if (!preg_match("/^number$/", $masque, $match)) {
+            return;
+        }
+
+        if (is_numeric($this->inputs[$key])) {
+            return;
+        }
+
+        $this->last_message = $this->lexical('number', $key);
+
+        $this->fails = true;
+
+        $this->errors[$key][] = [
+            "masque" => $masque,
+            "message" => $this->last_message
+        ];
+    }
+
+    /**
+     * Compile Int Rule
+     *
+     * [int] Check that the contents of the field is an integer number
+     *
+     * @param string $key
+     * @param string $masque
+     * @return void
+     */
+    protected function compileInt($key, $masque)
+    {
+        if (!preg_match("/^int$/", $masque, $match)) {
+            return;
+        }
+
+        if (is_int($this->inputs[$key])) {
+            return;
+        }
+
+        $this->last_message = $this->lexical('int', $key);
+
+        $this->fails = true;
+
+        $this->errors[$key][] = [
+            "masque" => $masque,
+            "message" => $this->last_message
+        ];
+    }
+
+    /**
+     * Compile Float Rule
+     *
+     * [float] Check that the field content is a float number
+     *
+     * @param string $key
+     * @param string $masque
+     * @return void
+     */
+    protected function compileFloat($key, $masque)
+    {
+        if (!preg_match("/^float$/", $masque, $match)) {
+            return;
+        }
+
+        if (is_float($this->inputs[$key])) {
+            return;
+        }
+
+        $this->last_message = $this->lexical('float', $key);
+
+        $this->fails = true;
+
+        $this->errors[$key][] = [
+            "masque" => $masque,
+            "message" => $this->last_message
+        ];
+    }
+}
---
+++ src/Validation/Rules/RegexRule.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Bow\Validation\Rules;
+
+trait RegexRule 
+{
+    /**
+     * Compile Regex Rule
+     *
+     * [regex] Check that the contents of the field with a regular expression
+     *
+     * @param string $key
+     * @param string $masque
+     * @return void
+     */
+    protected function compileRegex(string $key, string $masque): void
+    {
+        if (!preg_match("/^regex:(.+)+$/", $masque, $match)) {
+            return;
+        }
+
+        $regex = '~^' . $match[1] . '$~';
+
+        if (preg_match($regex, $this->inputs[$key])) {
+            return;
+        }
+
+        $this->fails = true;
+
+        $this->last_message = $this->lexical('regex', $key);
+
+        $this->errors[$key][] = [
+            "masque" => $masque,
+            "message" => $this->last_message
+        ];
+    }
+}
