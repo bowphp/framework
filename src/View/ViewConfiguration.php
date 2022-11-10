ec9de0db811a8ddedbce1d8285b62ec4def038a3
Franck DAKIA <dakiafranck@gmail.com>
2022-11-10 10:50:42 +0000

    Fix prototype compatibility

--- src/View/ViewConfiguration.php
+++ src/View/ViewConfiguration.php
@@ -12,7 +12,7 @@ class ViewConfiguration extends Configuration
     /**
      * @inheritdoc
      */
-    public function create(Loader $config)
+    public function create(Loader $config): void
     {
         /**
          * Configuration of view
@@ -27,7 +27,7 @@ class ViewConfiguration extends Configuration
     /**
      * @inheritdoc
      */
-    public function run()
+    public function run(): void
     {
         $this->container->make('view');
     }
