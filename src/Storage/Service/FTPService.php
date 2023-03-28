fa18b5b0bb92929f1c4c51ca090455c9b3d0df1b
Franck DAKIA <dakiafranck@gmail.com>
2023-03-28 19:45:12 +0000

    Fix get files from ftp error

--- src/Storage/Service/FTPService.php
+++ src/Storage/Service/FTPService.php
@@ -560,6 +560,7 @@ class FTPService implements ServiceInterface
         $normalizedListing = [];
 
         foreach ($listing as $child) {
+            $part = preg_split("/\s[0-9]{2}:[0-9]{2}\s/", $child);
             $chunks = preg_split("/\s+/", $child);
 
             list(
@@ -570,10 +571,10 @@ class FTPService implements ServiceInterface
                 $item['size'],
                 $item['month'],
                 $item['day'],
-                $item['time'],
-                $item['name']
+                $item['time']
             ) = $chunks;
 
+            $item["name"] = end($part);
             $item['type'] = $chunks[0][0] === 'd' ? 'directory' : 'file';
 
             array_splice($chunks, 0, 8);
