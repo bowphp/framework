da433b30a760a9b521f07bcad1fd621dd8e957f1
Franck DAKIA <dakiafranck@gmail.com>
2023-04-24 19:06:16 +0000

    Add policier configuration

--- tests/Config/stubs/policier.php
+++ tests/Config/stubs/policier.php
@@ -4,8 +4,8 @@ return [
     "signkey" => "FivwuTmpJlwfXB/WMjAyMS0wMS0yNCAyMDozMTozMTE2MTE1MjAyOTEuMDEwOA==",
 
     /**
-    * Token expiration time
-    */
+     * Token expiration time
+     */
     "exp" => 3600 * 24 * 3,
 
     /**
@@ -17,4 +17,36 @@ return [
      * Configures the audience
      */
     "aud" => app_env("APP_JWT_AUD", "app.example.com"),
+
+    /**
+     * Token is usable after this time
+     */
+    "nbf" => 60,
+
+    /**
+     * Token was issue
+     */
+    "iat" => 60,
+
+    /**
+     * Configures the issuer
+     */
+    "iss" => "localhost",
+
+    /**
+     * Configures the audience
+     */
+    "aud" => "localhost",
+
+    /**
+     * The type of the token, which is JWT
+     */
+    "typ" => "JWT",
+
+    /**
+     * Hashing algorithm being used
+     *
+     * HS256, HS384, HS512, RS256, RS384, RS512, ES256, ES384, ES512,
+     */
+    "alg" => "HS256",
 ];
