8999d124e42f3046c8edf8ed4f9b7624c5caa0c4
Franck DAKIA <dakiafranck@gmail.com>
2025-03-23 06:31:04 +0000

    fix: generate notification table command

--- src/Console/Command.php
+++ src/Console/Command.php
@@ -4,31 +4,32 @@ declare(strict_types=1);
 
 namespace Bow\Console;
 
-use Bow\Console\Command\AppEventCommand;
-use Bow\Console\Command\ClearCommand;
-use Bow\Console\Command\ConfigurationCommand;
-use Bow\Console\Command\ConsoleCommand;
-use Bow\Console\Command\ControllerCommand;
-use Bow\Console\Command\EventListenerCommand;
-use Bow\Console\Command\ExceptionCommand;
-use Bow\Console\Command\GenerateCacheCommand;
-use Bow\Console\Command\GenerateKeyCommand;
-use Bow\Console\Command\GenerateQueueCommand;
-use Bow\Console\Command\GenerateResourceControllerCommand;
-use Bow\Console\Command\GenerateSessionCommand;
-use Bow\Console\Command\MessagingCommand;
-use Bow\Console\Command\MiddlewareCommand;
-use Bow\Console\Command\MigrationCommand;
-use Bow\Console\Command\ModelCommand;
-use Bow\Console\Command\ProducerCommand;
+use ErrorException;
+use Bow\Support\Str;
 use Bow\Console\Command\ReplCommand;
+use Bow\Console\Command\ClearCommand;
+use Bow\Console\Command\ModelCommand;
 use Bow\Console\Command\SeederCommand;
 use Bow\Console\Command\ServerCommand;
-use Bow\Console\Command\ServiceCommand;
-use Bow\Console\Command\ValidationCommand;
 use Bow\Console\Command\WorkerCommand;
-use Bow\Support\Str;
-use ErrorException;
+use Bow\Console\Command\ConsoleCommand;
+use Bow\Console\Command\ServiceCommand;
+use Bow\Console\Command\AppEventCommand;
+use Bow\Console\Command\ProducerCommand;
+use Bow\Console\Command\ExceptionCommand;
+use Bow\Console\Command\MessagingCommand;
+use Bow\Console\Command\MigrationCommand;
+use Bow\Console\Command\ControllerCommand;
+use Bow\Console\Command\MiddlewareCommand;
+use Bow\Console\Command\ValidationCommand;
+use Bow\Console\Command\GenerateKeyCommand;
+use Bow\Console\Command\ConfigurationCommand;
+use Bow\Console\Command\EventListenerCommand;
+use Bow\Console\Command\GenerateCacheCommand;
+use Bow\Console\Command\GenerateQueueCommand;
+use Bow\Console\Command\GenerateSessionCommand;
+use Bow\Console\Command\GenerateNotificationCommand;
+use Bow\Console\Command\GenerateResourceControllerCommand;
 
 class Command extends AbstractCommand
 {
@@ -63,7 +64,7 @@ class Command extends AbstractCommand
             "session-table" => GenerateSessionCommand::class,
             "queue-table" => GenerateQueueCommand::class,
             "cache-table" => GenerateCacheCommand::class,
-            "notification-table" => GenerateCacheCommand::class,
+            "notification-table" => GenerateNotificationCommand::class,
         ],
         "runner" => [
             "console" => ReplCommand::class,
